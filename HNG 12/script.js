56c87c302b2acd0174fecb4aaea6915573e8cb4e
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2025-02-03 11:20:10 +0100

    Delete HNG 12 directory

--- HNG 12/script.js
+++
@@ -1,8 +0,0 @@
-function updateUTCTime() {
-    const utcTimeElement = document.getElementById("utc-time");
-    const now = new Date();
-    utcTimeElement.textContent = `Current UTC Time: ${now.toUTCString()}`;
-}
-
-updateUTCTime();
-setInterval(updateUTCTime, 1000);
